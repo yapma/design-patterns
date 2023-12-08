d7e2687f17e1fadcc643782a9273f1ecf2c52aa4
yapma <yapmayashar@gmail.com>
2023-12-08 23:19:44 +0330

    - add singleton pattern.

---
+++ DesignPatterns/Creational/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VoteMachine vm1 = VoteMachine.Instance;
+            VoteMachine vm2 = VoteMachine.Instance;
+            VoteMachine vm3 = VoteMachine.Instance;
+
+            vm1.RegisterVote();
+            vm2.RegisterVote();
+            vm3.RegisterVote();
+
+            Console.WriteLine(vm1.TotalVotes);
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/VoteMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class VoteMachine
+    {
+        private static VoteMachine _instance = null;
+        private int _totalVotes = 0;
+
+        private VoteMachine()
+        {
+        }
+
+        public static VoteMachine Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new VoteMachine();
+
+                return _instance;
+            }
+        }
+
+
+        public void RegisterVote()
+        {
+            _totalVotes += 1;
+            Console.WriteLine("Registered Vote #" + _totalVotes);
+        }
+
+        public int TotalVotes
+        {
+            get
+            {
+                return _totalVotes;
+            }
+        }
+    }
+}
